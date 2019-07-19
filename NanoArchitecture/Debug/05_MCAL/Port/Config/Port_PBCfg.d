bcca8fdf57fdc3e46a9fe7ee81f1f71dbefc69db
vladBaciu <baciuvladeusebiu@gmail.com>
2019-07-19 23:59:07 +0300

    added port in mcal layer

---
+++ NanoArchitecture/Debug/05_MCAL/Port/Config/Port_PBCfg.d
@@ -0,0 +1,50 @@
+05_MCAL/Port/Config/Port_PBCfg.d 05_MCAL/Port/Config/Port_PBCfg.o: \
+ ../05_MCAL/Port/Config/Port_PBCfg.c \
+ ../07_Libraries/RegisterInit/Generic/RegisterInit.h \
+ ../07_Libraries/Std/Generic/Code/Std_Types.h \
+ ../07_Libraries/Std/Generic/Code/Platform_Types.h \
+ ../07_Libraries/Std/Generic/Code/Compiler.h \
+ ../05_MCAL/Port/Generic/Port.h \
+ d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ D:\Programe\AtmelStudio\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iom328p.h \
+ d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
+
+../07_Libraries/RegisterInit/Generic/RegisterInit.h:
+
+../07_Libraries/Std/Generic/Code/Std_Types.h:
+
+../07_Libraries/Std/Generic/Code/Platform_Types.h:
+
+../07_Libraries/Std/Generic/Code/Compiler.h:
+
+../05_MCAL/Port/Generic/Port.h:
+
+d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+D:\Programe\AtmelStudio\7.0\Packs\atmel\ATmega_DFP\1.2.209\include/avr/iom328p.h:
+
+d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\programe\atmelstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
