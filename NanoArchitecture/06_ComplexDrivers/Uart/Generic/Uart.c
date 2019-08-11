22e30d5ba9215084836229d3f2c91e93e4524db3
vladBaciu <baciuvladeusebiu@gmail.com>
2019-08-11 22:28:32 +0300

    Added UART generic functions

--- NanoArchitecture/06_ComplexDrivers/Uart/Generic/Uart.c
+++ NanoArchitecture/06_ComplexDrivers/Uart/Generic/Uart.c
@@ -1,21 +1,26 @@
 /*-------------------------------------------------------------------------------------------------------------------*/
 /**
- *    \file       Uart.c
- *    \author     Baciu Vlad-Eusebiu
- *    \brief      Implements all the functions for UART module.
- *
- *
- */
+*    \file       Uart.c
+*    \author     Baciu Vlad-Eusebiu
+*    \brief      Implements all the functions for UART module.
+*
+*
+*/
 /*-------------------------------------------------------------------------------------------------------------------*/
 
 /*-------------------------------------------------------------------------------------------------------------------*/
 /*                                                     Inclusions                                                    */
 /*-------------------------------------------------------------------------------------------------------------------*/
 #include "Uart.h"
+#include <avr/io.h>
 /*-------------------------------------------------------------------------------------------------------------------*/
 /*                                             Definition Of Local Macros                                            */
 /*-------------------------------------------------------------------------------------------------------------------*/
-
+#define UART_TX_STATUS_BIT			    (1<<6)
+#define UART_RX_STATUS_BIT			    (1<<7)
+#define UART_STATUS_REGISTER			(0U)
+#define UART_RECEIVE_REGISTER			(1U)
+#define UART_TRANSMIT_REGISTER			(2U)
 /*-------------------------------------------------------------------------------------------------------------------*/
 /*                                           Definition Of Local Data Types                                          */
 /*-------------------------------------------------------------------------------------------------------------------*/
@@ -49,65 +54,92 @@ static Uart_GroupCfgType * Uart_pt_GroupsConfig;
 /*                                         Implementation Of Global Functions                                        */
 /*-------------------------------------------------------------------------------------------------------------------*/
 /**
- * \brief   Init the UART channels from the configuration array
- * \param   pt_Config: pointer to the configuration array
- * \return  -
- */
+* \brief   Init the UART channels from the configuration array
+* \param   pt_Config: pointer to the configuration array
+* \return  -
+*/
 void Uart_gv_Init(const Uart_ConfigType * pt_Config)
 {
-   RegInit_gv_Masked8Bits(pt_Config->pt_ConfigRegisters);
-   Uart_pt_GroupsConfig = (Uart_GroupCfgType*) pt_Config->pt_ConfigGroup;
+	RegInit_gv_Masked8Bits(pt_Config->pt_ConfigRegisters);
+	Uart_pt_GroupsConfig = (Uart_GroupCfgType*) pt_Config->pt_ConfigGroup;
 
 }
 
 #if(UART_TRANSMIT_API == STD_ON)
 /**
- * \brief   Reads the transmision register status.
- * \param   t_ChannelId: index of UART channel in the configuration array
- * \return  The status of transmit operation: ready or busy.
- */
+* \brief   Reads the transmision register status.
+* \param   t_ChannelId: index of UART channel in the configuration array
+* \return  The status of transmit operation: ready or busy.
+*/
 Uart_TxStatusType Uart_gt_GetTransmitStatus(Uart_ChannelType t_ChannelId)
 {
-   Uart_TxStatusType t_ReturnValue;
-   
-   return t_ReturnValue;
+	Uart_TxStatusType t_ReturnValue;
+	uint8 uc_StatusRegister;
+	uc_StatusRegister = (uint8) *Uart_pt_GroupsConfig[t_ChannelId].pt_Registers[UART_STATUS_REGISTER];
+	if((uc_StatusRegister & UART_TX_STATUS_BIT) !=0)
+	{
+		t_ReturnValue = UART_TX_READY;
+		
+	}
+	else
+	{
+		t_ReturnValue = UART_TX_BUSY;
+	}
+	return t_ReturnValue;
 }
 
 /**
- * \brief   Puts data in the DR register.
- * \param   t_ChannelId: index of UART channel in the configuration array.
- * \param   puc_TransmitAddr: buffer pointer for the data that is transmitted.
- * \return  -
- */
+* \brief   Puts data in the DR register.
+* \param   t_ChannelId: index of UART channel in the configuration array.
+* \param   puc_TransmitAddr: buffer pointer for the data that is transmitted.
+* \return  -
+*/
 void Uart_gv_Transmit(Uart_ChannelType t_ChannelId, uint8 * puc_TransmitAddr)
 {
-  
+	  *Uart_pt_GroupsConfig[t_ChannelId].pt_Registers[UART_TRANSMIT_REGISTER] = (uint8) *puc_TransmitAddr;
+ while (Uart_gt_GetTransmitStatus(t_ChannelId) == UART_TX_BUSY)
+ {
 
+ };
 }
 #endif
 
 #if(UART_RECEIVE_API == STD_ON)
 /**
- * \brief   Reads the receive register status.
- * \param   t_ChannelId: index of UART channel in the configuration array
- * \return  The status of receive operation: ready or busy.
- */
+* \brief   Reads the receive register status.
+* \param   t_ChannelId: index of UART channel in the configuration array
+* \return  The status of receive operation: ready or busy.
+*/
 Uart_RxStatusType Uart_gt_GetReceiveStatus(Uart_ChannelType t_ChannelId)
 {
-   Uart_TxStatusType t_ReturnValue;
-   
-   return t_ReturnValue;
+	Uart_RxStatusType t_ReturnValue;
+	uint8 uc_StatusRegister;
+	uc_StatusRegister = (uint8) *Uart_pt_GroupsConfig[t_ChannelId].pt_Registers[UART_STATUS_REGISTER];
+	if((uc_StatusRegister & UART_RX_STATUS_BIT) !=0)
+	{
+		t_ReturnValue = UART_RX_READY;
+		
+	}
+	else
+	{
+		t_ReturnValue = UART_RX_NO_DATA;
+	}
+	return t_ReturnValue;
 
 }
 
 /**
- * \brief   Reads data from the DR register.
- * \param   t_ChannelId: index of UART channel in the configuration array.
- * \param   puc_ReceiveAddr: buffer pointer where incoming data is stored.
- * \return  -
- */
+* \brief   Reads data from the DR register.
+* \param   t_ChannelId: index of UART channel in the configuration array.
+* \param   puc_ReceiveAddr: buffer pointer where incoming data is stored.
+* \return  -
+*/
 void Uart_gv_Receive(Uart_ChannelType t_ChannelId, uint8 * puc_ReceiveAddr)
 {
- 
+	while (Uart_gt_GetReceiveStatus(t_ChannelId) == UART_RX_NO_DATA)
+	{
+
+	};
+	*puc_ReceiveAddr =  (uint8) *Uart_pt_GroupsConfig[t_ChannelId].pt_Registers[UART_RECEIVE_REGISTER];
 }
 #endif
